514b89e37d1b15b1d19bf57e4fceeddcffd18577
Jonathan Sheng <js165@rice.edu>
2020-04-09 22:38:48 -0400

    added winning system, need more features

--- backend.js
+++ backend.js
@@ -1,4 +1,3 @@
-//Testing
 var deck = {'A': 4, 'K': 4, 'Q': 4, 'J': 4, 10 : 4, 9 : 4, 8 : 4, 7 : 4, 6 : 4, 5 : 4, 4 : 4, 3 : 4, 2 : 4};
 var lst_suits = ['Diamonds', 'Clubs', 'Hearts', 'Spades'];
 var suits = {'A': [...lst_suits], 'K': [...lst_suits], 'Q': [...lst_suits], 'J': [...lst_suits], 10 : [...lst_suits], 9 : [...lst_suits], 8 : [...lst_suits], 7 : [...lst_suits], 6 : [...lst_suits], 5 : [...lst_suits], 4 :  [...lst_suits], 3 : [...lst_suits], 2 : [...lst_suits]};
@@ -48,7 +47,7 @@ function dealCards(){//Deals Card
         }
         else{
             dealer += ' ' + card_drawn_lst[0] + ' of ' + card_drawn_lst[1] + ' | ';
-            dealer_cards.push(card_drawn_lst[1])
+            dealer_cards.push(card_drawn_lst[0])
             cardPicture(card_drawn_lst[0],card_drawn_lst[1],false,dealer_cards.length);
         }
     }
@@ -83,9 +82,10 @@ function resetGame(){//Reset all variables, except score
 // stand returning weird values
 function stand(){ //sum up current hold
     var counter = 0;
+    var hasAce = false;
     for (index =0; index < player_cards.length; index ++){
         if (player_cards[index] == 'A'){
-            counter += 11
+            hasAce = True;
         }
         else if (player_cards[index] == 'K' || player_cards[index] == 'Q' || player_cards[index] == 'J'){
             counter += 10;
@@ -94,8 +94,51 @@ function stand(){ //sum up current hold
             counter += parseInt(player_cards[index])
         }
     }
-    console.log(counter)
-    return counter
+    if (hasAce){
+        if (counter + 11 > 21){
+            counter += 1
+        }
+        else{
+            counter += 11
+        }
+    }
+    console.log("Sum of player hand " + counter)
+    dealerTurn(counter)
+}
+function dealerTurn(player_value){
+    var hasAce = false;
+    var counter = 0;
+    for (index = 0; index < dealer_cards.length; index ++){
+        if (dealer_cards[index] == 'A'){
+            hasAce = True;
+        }
+        else if (dealer_cards[index] == 'K' || dealer_cards[index] == 'Q' || dealer_cards[index] == 'J'){
+            counter += 10;
+        }
+        else{
+            counter += parseInt(dealer_cards[index])
+        }
+    }
+    if (hasAce){
+        if ((counter + 11 <= 21) && (counter + 11)>= 17){
+            counter += 11
+        }
+        else{
+            counter += 1
+        }
+    }
+    console.log("Sum of dealer hand "+ counter)
+    if (counter >= 17){
+        if (player_value > counter){
+            document.getElementById('result').innerHTML = ('Result: You Win!');
+        }
+        else if (player_value == counter){
+            document.getElementById('result').innerHTML = ('Result: A Push!');
+        }
+        else{
+            document.getElementById('result').innerHTML = ('Result: Dealer Wins :(');
+        }
+    }
 }
 function cardPicture(cardNum,suit,playerOrDealer,numCard){
     //if playerOrComp is true, means it's the player's card. If false, means it's the dealer's card
