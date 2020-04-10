9a861d6fbf054217bd07642857b24eb24436ce1c
JSheng1689 <55417691+JSheng1689@users.noreply.github.com>
2020-04-09 22:17:29 -0400

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,3 +1,4 @@
+//Testing
 var deck = {'A': 4, 'K': 4, 'Q': 4, 'J': 4, 10 : 4, 9 : 4, 8 : 4, 7 : 4, 6 : 4, 5 : 4, 4 : 4, 3 : 4, 2 : 4};
 var lst_suits = ['Diamonds', 'Clubs', 'Hearts', 'Spades'];
 var suits = {'A': [...lst_suits], 'K': [...lst_suits], 'Q': [...lst_suits], 'J': [...lst_suits], 10 : [...lst_suits], 9 : [...lst_suits], 8 : [...lst_suits], 7 : [...lst_suits], 6 : [...lst_suits], 5 : [...lst_suits], 4 :  [...lst_suits], 3 : [...lst_suits], 2 : [...lst_suits]};
